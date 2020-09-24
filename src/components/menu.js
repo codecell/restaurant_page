5278ebdb88bc97f23f6ccbea5eb7e0abff20eeba
Ezaka Alfred <ezakaphil@gmail.com>
2020-09-25 00:43:10 +0100

    Add prices to menu items

--- src/components/menu.js
+++ src/components/menu.js
@@ -5,10 +5,10 @@ import pearImg from '../images/pear.png';
 import cutleryImg from '../images/cutlery.png';
 
 const dayMenu = [
-  { image: chowImg, title: 'Continental' },
-  { image: pearImg, title: 'Health Booze' },
-  { image: burgerImg, title: 'Best Bite' },
-  { image: cutleryImg, title: 'Custom Mix' },
+  { image: chowImg, title: 'Continental', price: '£100' },
+  { image: pearImg, title: 'Health Booze', price: '£40' },
+  { image: burgerImg, title: 'Best Bite', price: '£80' },
+  { image: cutleryImg, title: 'Custom Mix', price: '£180' },
 ];
 
 const menu = `
@@ -18,7 +18,10 @@ const menu = `
       ${dayMenu.map((item) => `
           <div class="card">
             <img src=${item.image} class="menu-img" />
-            <p class="food-label">${item.title}</p>
+            <p class="food-label">
+              ${item.title}
+              <span class="price">${item.price}</span>
+            </p>
           </div>
         `)}
     </div>
